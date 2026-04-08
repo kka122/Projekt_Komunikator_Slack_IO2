2be076ed0fc952ee2103bbb38b52f0c5f753ad27
Damian Osiński <osinskidamian03@gmail.com>
2026-04-08 04:11:34 +0200

    feat: add openapi for auth

---
+++ shared/generators/orval/orval.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'orval';
+
+export default defineConfig({
+  szponcik: {
+    input: '../../openapi/openapi.yaml',
+    output: {
+      mode: 'tags-split',
+      target: '../../../app/schemas/endpoints',
+      schemas: '../../../app/schemas/models',
+      client: 'react-query',
+      mock: true,
+      override: {
+        query: {
+          useQuery: true,
+          useInfinite: true,
+        }
+      }
+    },
+  },
+  szponcikZod: {
+    input: {
+      target: '../../openapi/openapi.yaml',
+    },
+    output: {
+      mode: 'tags-split',
+      client: 'zod',
+      target: '../../../app/schemas/endpoints',
+      fileExtension: '.zod.ts',
+    },
+  }
+});
