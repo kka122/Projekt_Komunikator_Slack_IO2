e206b9930120b00076845286732abd185002a93a
Damian Osiński <osinskidamian03@gmail.com>
2026-04-14 13:28:19 +0200

    feat: https never exist

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     },
     proxy: {
       '/api': {
-        target: 'https://localhost:5000',
+        target: 'http://localhost:5000',
         changeOrigin: true,
       },
       '/socket': {
