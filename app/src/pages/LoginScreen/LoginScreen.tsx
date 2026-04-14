75407fa00c72f8cc300957708221b25c6bf675f6
Damian Osiński <osinskidamian03@gmail.com>
2026-04-14 14:55:52 +0200

    feat: add basic google auth fetch

--- app/src/pages/LoginScreen/LoginScreen.tsx
+++ app/src/pages/LoginScreen/LoginScreen.tsx
@@ -4,7 +4,23 @@ import {GoogleLogin} from "@react-oauth/google";
 function LoginScreen(): JSX.Element {
   return <>
     <GoogleLogin onSuccess={credentialResponse => {
-      console.log(credentialResponse)
+      fetch("/api/auth/google", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          token: credentialResponse.credential
+        })
+      }).then(res => {
+        if (res.ok) {
+          console.log("Login Successful")
+        } else {
+          console.log("Login Failed")
+        }
+      }).catch(() => {
+        console.log("Login Failed")
+      })
     }} onError={() => {
       console.log("Login Failed")
     }}/>
