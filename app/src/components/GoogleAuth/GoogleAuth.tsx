2a5b75d2511c4c2cf276366db12dab8230c2d2fc
Damian Osiński <osinskidamian03@gmail.com>
2026-06-09 04:39:20 +0200

    feat: and functionality to google auth component

--- app/src/components/GoogleAuth/GoogleAuth.tsx
+++ app/src/components/GoogleAuth/GoogleAuth.tsx
@@ -1,24 +1,48 @@
-import {type JSX} from "react";
-import {useGoogleLogin} from "@react-oauth/google";
+import {type JSX, useRef} from "react";
+import {type CredentialResponse, GoogleLogin} from "@react-oauth/google";
 import styles from './GoogleAuth.module.css'
 import InlineHotkey from "../InlineHotkey/InlineHotkey.tsx";
-// import type {GoogleAuthRequestBody} from "../../api/models";
+import type {GoogleAuthRequestBody} from "../../api/models";
+import {GoogleAuthBody} from "../../api/endpoints/auth/auth.zod.ts";
+import {googleAuth} from "../../api/endpoints/auth/auth.ts";
 
 function GoogleAuth(): JSX.Element {
-  const login = useGoogleLogin({
-    onSuccess: (tokenResponse) => {
-      console.log(tokenResponse);
-      // const requestBody: GoogleAuthRequestBody = {
-      //   token: tokenResponse.access_token,
-      // }
-      // const requestBodyParsed =
-    },
-  })
+  const googleLoginRef = useRef<HTMLDivElement>(null);
+
+  const login = (credentialResponse: CredentialResponse) => {
+    if (!credentialResponse.credential) return;
+    const requestBody: GoogleAuthRequestBody = {
+      token: credentialResponse.credential
+    }
+    const requestBodyParsed = GoogleAuthBody.safeParse(requestBody);
+    if (!requestBodyParsed.success) {
+      throw new Error('Invalid request body');
+    }
+
+    googleAuth(requestBodyParsed.data)
+      .then(response => {
+        console.log('Login successful', response);
+      })
+      .catch(error => {
+        console.error(error);
+      })
+  }
+
+  const triggerGoogleLogin = () => {
+    // Find the Google Login button inside the div and click it
+    const button = googleLoginRef.current?.querySelector('div[role="button"]') as HTMLElement;
+    if (button) {
+      button.click();
+    }
+  }
 
   return <div className={styles.googleAuth}>
-    <InlineHotkey hotkeyFunction={login} hotkeyKey={'G'} letterIndex={1}>
+    <InlineHotkey hotkeyFunction={triggerGoogleLogin} hotkeyKey={'G'} letterIndex={1}>
       Zaloguj się przez Google
     </InlineHotkey>
+    <div ref={googleLoginRef} className={styles.googleButton}>
+      <GoogleLogin onSuccess={login} onError={() => console.log('Login Failed')}/>
+    </div>
   </div>
 }
 
